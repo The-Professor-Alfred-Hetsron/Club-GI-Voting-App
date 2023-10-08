31f3eb4d723d0a1db70177555899fcdd873a473a
The-Professor-Alfred-Hetsron <parkeryepnjio@gmail.com>
2023-10-08 18:52:23 +0100

    Project Installation

---
+++ tailwind.config.ts
@@ -0,0 +1,79 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'primary': '#F2AA06',
+        'secondary': '#009DDC',
+        'tertiary': '#0958A9',
+        'lightBlue': '#E8F2F8',
+        'whiteTone': '#F9F9F9',
+        'grayToneStroke': '#B8BFCC',
+        'grayTone1': '#3C404B',
+        'grayTone2': '#6C768A',
+        'grayTone3': '#A0ABC0',
+        'textDarkTone': '#23252F',
+      },
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }  
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }  
+        }
+      },
+      animation: {
+        typing: "typing 2s steps(20) , blink .7s infinite"
+      },
+    },
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
+  },
+  plugins: [],
+}
+export default config
